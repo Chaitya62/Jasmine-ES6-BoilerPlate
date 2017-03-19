09e7b9079276e4c810ef231127a91954f3cd1172
Chaitya Shah <Chaitya62@gmail.com>
2017-03-19 16:05:40 +0530

    Chaitya is great

---
+++ spec/hn.spec.js
@@ -0,0 +1,12 @@
+describe('Promise',()=>{
+
+	it('should return a promise',()=>{
+		var promise = returnsPromise();
+
+		expect(typeof promise.then).toBe('function');
+		expect(typeof promise.catch).toBe('function');
+	});
+
+
+
+});
---
+++ src/hn.js
@@ -0,0 +1,8 @@
+let returnsPromise = ()=>{
+	let p = new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			return 'hahah';
+		},1000);
+	});
+	return p;
+}
